044d846ec4dd8caf877db92df317fda63a992971
fazlay <rfazlay21@gmail.com>
2021-12-05 23:41:47 +0600

    simple

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       <h1 className="my-5 fs-1 fw-bold text-primary">
         TOP CONTRIBUTIOR OF THIS MONTH
       </h1>
-
       <HomePage></HomePage>
       <Footer></Footer>
     </div>
